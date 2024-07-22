c10a34aec23686d6f33ee66d19966927d48101d5
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-07-22 22:19:18 +0700

    [PHP][Customer] - Controller ,route & model

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $customers = Customer::where('status',1)->paginate(10);
+        if ($request->ajax()) {
+            return view('pages.customers.pagination', compact('customers'))->render();
+        }
+
+        return view('pages.customers.index', [
+            'customers' => $customers
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = Customer::create($request->all());
+
+        if ($request->ajax()) {
+            $customers = Customer::paginate(10);
+            return view('pages.customers.pagination', compact('customers'))->render();
+        }
+
+        return redirect('/customers');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

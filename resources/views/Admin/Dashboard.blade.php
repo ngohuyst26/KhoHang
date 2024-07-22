c2eb19eacf4b9fd308d9d747eb02b6e6f8ef538b
VinhNTpc <vinhntpv03157@fpt.edu.vn>
2024-07-22 13:07:11 +0700

    [Admin] - Giao diện admin

---
+++ resources/views/Admin/Dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('client.layouts.master')
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.dashboard.index');
+    return view('welcome');
+});
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
 });
 
 Route::resource('customers', CustomerController::class);

55941fb97c6fd3ab61793d1e4de4986c4d8a3c33
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-07-24 18:51:45 +0700

    [PHP][Customer] - Model & Controller

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
@@ -13,7 +14,7 @@ class CustomerController extends Controller
      */
     public function index(Request $request)
     {
-        $customers = Customer::where('status',1)->paginate(10);
+        $customers = Customer::orderBy('id','desc')->where('status','1')->paginate(10);
         if ($request->ajax()) {
             return view('pages.admin.customers.pagination', compact('customers'))->render();
         }
@@ -36,14 +37,26 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ],
+        [
+            'required' => 'Dữ liệu không được trống!',
+            'max' => 'Dữ liệu'
+        ]);
+
+        if ($validator->fails()) {
+            if ($request->ajax()) {
+                return response()->json(['errors' => $validator->errors()]);
+            }
+        }
         $product = Customer::create($request->all());
 
         if ($request->ajax()) {
-            $customers = Customer::paginate(10);
-            return view('pages.admin.customers.pagination', compact('customers'))->render();
+            $customers = Customer::orderBy('id','desc')->where('status','1')->paginate(10);
+            return view('pages.admin.customers.pagination', ['customers'=>$customers])->render();
         }
-
-        return redirect('/customers');
+        return redirect('customers.index');
     }
 
     /**
@@ -59,7 +72,8 @@ class CustomerController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $customers = Customer::findOrFail($id);
+        return response()->json($customers);
     }
 
     /**
@@ -67,7 +81,50 @@ class CustomerController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        if ($request->status !== null){
+            $acreage_range         = AcreageRange::findOrFail($id);
+            $acreage_range->status = $request->status;
+            $acreage_range->save();
+
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+        }
+
+        $validator = Validator::make($request->all(),
+            [
+                'name'       => 'required|string|max:255',
+                'min_acreage'  => 'required|integer|min:0',
+                'max_acreage'  => 'required|integer|gt:min_acreage',
+            ],
+            [
+                'required'       => 'Dữ liệu không được trống!',
+                'integer'        => 'Gia trị phải là số',
+                'name.string'    => 'Giá trị phải là chuổi!',
+                'name.max'       => 'Giá trị phải nhỏ hơn :max kí tự!',
+                'min_acreage.min'  => 'Giá trị phải nhỏ hơn :min kí tự!',
+                'gt'            => 'Giá max phải lớn hơn giá min!'
+            ]
+        );
+
+        if ($validator->passes()) {
+            $acreage_range              = AcreageRange::findOrFail($id);
+            $acreage_range->name        = $request->name;
+            $acreage_range->min_acreage = $request->min_acreage;
+            $acreage_range->max_acreage = $request->max_acreage;
+            $acreage_range->save();
+
+            return response()->json([
+                'status' => true,
+                'errors' => [],
+            ]);
+        }
+
+        return response()->json([
+            'status' => false,
+            'errors' => $validator->errors(),
+        ]);
     }
 
     /**
@@ -75,6 +132,11 @@ class CustomerController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $acreage_range = AcreageRange::findOrFail($id);
+        $acreage_range->delete();
+
+        return response()->json([
+            'status' => true
+        ]);
     }
 }

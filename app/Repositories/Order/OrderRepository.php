4012184d62354b18b5639ac014f35ff0ce01364c
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-11-12 09:56:51 +0700

    [API][Order] - Add order_items & user.order for orders

--- app/Repositories/Order/OrderRepository.php
+++ app/Repositories/Order/OrderRepository.php
@@ -21,7 +21,7 @@ class OrderRepository extends BaseRepository implements OrderRepositoryInterface
 
     public function filter($keyword = '',$limit = 10, $status = '1')
     {
-        $brands = $this->model->orderBy('id', 'DESC');
+        $brands = $this->model->orderBy('id', 'DESC')->with('orderItems','user.orders');
 
         if(!empty($keyword)){
             $brands = $brands->where('name','like','%'.$keyword.'%');

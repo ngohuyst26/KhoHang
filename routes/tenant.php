a102d7a10d669023e2d887106803d5f571c49180
ngohuy26 <79693723+ngohuyst26@users.noreply.github.com>
2024-11-22 14:10:04 +0700

    [API]- API trang dashboard

--- routes/tenant.php
+++ routes/tenant.php
@@ -78,8 +78,7 @@ Route::middleware([
     Route::middleware(['auth:api', VerifyTenantToken::class])->group(function (){
         Route::group(['middleware' => ['role:admin']], function (){
 
-            Route::get('/dashboard/doanh-thu', [DashboardController::class, 'getDoanhThu']);
-            Route::get('/dashboard/loi-nhuan', [DashboardController::class, 'getLoiNhuan']);
+            Route::get('/dashboard', [DashboardController::class, 'dashboard']);
 
             Route::post('introductions', [IntroductionController::class, 'store']);
             Route::get('introductions', [IntroductionController::class, 'show']);
